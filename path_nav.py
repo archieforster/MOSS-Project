aecec5b0f93611bbf7b17cad0ad3ead6615240d3
dhruvgupta3126 <101194006+dhruvgupta3126@users.noreply.github.com>
2024-11-18 15:12:34 +0000

    Update path_nav.py

--- path_nav.py
+++ path_nav.py
@@ -275,7 +275,10 @@ class Navigator:
     
     def getNoEvacuatingPeople(self):
         return self.total_evacuees
-    
+
+    def getNoEvacuatedPeople(self):
+    return self.evacuated_people
+
     def getAvgNoPeoplePerCar(self):
         if len(self.car_states) > 0:
             return self.total_evacuees / len(self.car_states)
